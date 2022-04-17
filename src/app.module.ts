8d43bcdc5df72c0d1d7a7715b0e9870ce9ba6185
JiHyun, Jeong <39480072+dinb1242@users.noreply.github.com>
2022-04-17 20:31:20 +0900

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ExampleModule } from './example/example.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`, `.env.${process.env.NODE_ENV}`],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ExampleModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
